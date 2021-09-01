a93d9e9f026218145ab8a1893a2d6371d65ea875
mikejguo290 <mikejguo290@gmail.com>
2021-09-01 13:20:28 +0100

    create options for createSlice.

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const options = {
+    name:'topics',
+    initialState: {
+        topics:{}
+    },
+    reducers:{
+        addTopic:(state,action)=>{
+            const {id, name, icon} = action.payload;
+            const topic = { // ...action.payload is more succint but less clear. 
+                id: id,
+                name: name,
+                icon: icon, 
+                quizIds:[], // list of quiz ids that belong to this topic.
+            }
+            state.topics[id]=topic;
+        }
+    }
+}
