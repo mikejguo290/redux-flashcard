1c9a7d0d5a482453210b5b2a73d21536a7d2800a
mikejguo290 <mikejguo290@gmail.com>
2021-09-01 21:48:33 +0100

    create a thunk action creator to dispatch addQuiz and addQuizId(of topicsSlice) together in thunk.

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { addQuizId } from '../topics/topicsSlice'
 
 const options = {
     name: 'quizzes', 
@@ -23,4 +24,24 @@ export const selectQuizzes = state => state.quizzes.quizzes;
 
 const quizzesSlice = createSlice(options);
 export const { addQuiz } = quizzesSlice.actions;
+
+//Conceptually, the actions of creating a new quiz and associating it with its topic are a part of a single process. 
+
+export const addQuizAndTopicId = (payload)=>{
+    return (dispatch)=>{
+        // the thunk groups the acts of dispatching two action creators -  addQuiz and addQuizId(toTopic) A.C, together. 
+        // (aside. the two action creators are from different slices here!)
+        // dispatching thunk a.c. doesn't call it immediately. the execution is delayed because it is passed to middleware first.
+        dispatch(addQuiz(payload));
+        // adapt addQuiz's payload for addQuizId. 
+        const { id, topicId } = payload;
+        const input = {
+            topicId: topicId, 
+            quizId: id,
+        }
+        // expect { topicId, quizId } from payload
+        dispatch(addQuizId(input)); //change  quizId to id on addQuizId on topicsSlice.js
+    }
+}
+
 export default quizzesSlice.reducer;
